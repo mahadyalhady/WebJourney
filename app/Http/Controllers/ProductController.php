736c336f5d7578c2295a754ac3eb957fdfff5e2c
Mahady Al Hady <mahady426@gmail.com>
2023-06-11 01:57:45 +0600

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index($var=null){
-        return 'all products'.$var;
+        return 'all products '.$var;
     }
 }
