30c8af0470ecccfb06ce58d2739267adee250ca3
Mahady Al Hady <mahady426@gmail.com>
2023-06-11 18:45:51 +0600

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,7 @@
 <div>
+    <h2>About</h2>
+    <h2>Product</h2>
+    <h2>Contact</h2>
+    <h2>Service</h2>
     <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
 </div>
