d9fd812f91d5b0ad8795168da2279174d47ffddd
Mahady Al Hady <mahady426@gmail.com>
2023-06-12 18:46:05 +0600

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Http;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+
+// use Illuminate\Support\Facades\Http;
 
 
 
@@ -18,13 +19,26 @@ class PostController extends Controller
 
     public function apipostsingle($id)
     {
-        $apipostsingle = Http::get('https://jsonplaceholder.typicode.com/posts/' . $id);
-        return $apipostsingle->json();
+        return 'ok';
+        // $apipostsingle = Http::get('https://jsonplaceholder.typicode.com/posts/' . $id);
+        // return $apipostsingle->json();
     }
 
     public function addpost()
     {
         $addpost = http::post('https://jsonplaceholder.typicode.com/posts', ['userId' => 4, 'title' => 'new title', 'body' => 'new details']);
         return $addpost->json();
+    }
+
+    public function editpost($id)
+    {
+        $editpost = Http::put('https://jsonplaceholder.typicode.com/posts/' . $id, ['userId' => 4, 'title' => 'new title updated', 'body' => 'new details updated']);
+        return $editpost->json();
+    }
+
+    public function deletepost($id)
+    {
+        $deletepost = Http::delete('https://jsonplaceholder.typicode.com/posts/' . $id, );
+        return $deletepost->json();
     }
 }
