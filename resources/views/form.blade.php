69ab9850d7175ac444dd684dd62a2448c8c80b09
Mahady Al Hady <mahady426@gmail.com>
2023-06-13 18:06:04 +0600

    Update form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,20 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <title>Document</title>
 </head>
 <body>
     <div class="container">
-        <form action="{{ route('formsubmit') }}" method="POST">
+
+        {{-- error message --}}
+        {{-- @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif --}}
+
+        <form action="{{ route('formvalidation') }}" method="POST">
             @csrf
             <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
+                <label class="form-label">Name </label>
+                <input type="text" name="name" class="form-control" >
+                @error('name')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+              </div>
+
             <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
+              <label class="form-label">Email address</label>
+              <input type="email" class="form-control" >
+              @error('email')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label">Password</label>
+              <input type="password" class="form-control" >
             </div>
             
             <button type="submit" class="btn btn-primary">Submit</button>
