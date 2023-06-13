f88f7690f7c9f49b08eeaa8cc3b72d52b560b12a
Mahady Al Hady <mahady426@gmail.com>
2023-06-13 18:05:57 +0600

    Update FormController.php

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Mime\Email;
 
 class FormController extends Controller
 {
@@ -14,6 +15,22 @@ class FormController extends Controller
     public function formsubmit(Request $request)
     {
         // return 'Form Submit Successful';
+
+        // warning message
+        $rules = [
+            'name' => 'required|max:20',
+            'email' => 'required|email'
+        ];
+
+        // custom message create
+        $custommessage = [
+            'name.required' => 'enter your name',
+            // 'name.required' => "you can't use  five character for your name",
+            'email.required' => 'enter your email',
+            'email.email' => 'email must be valid'
+        ];
+
+        $this->validate($request, $rules, $custommessage);
         return $request->all();
     }
 
