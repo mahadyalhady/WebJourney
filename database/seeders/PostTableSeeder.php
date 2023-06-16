f2ba841f1856ab5e73de27db47583df3e8e6a146
Mahady Al Hady <mahady426@gmail.com>
2023-06-17 04:50:06 +0600

    Update PostTableSeeder.php

--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class PostTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $var = [
+            ['id' => 1, 'title' => 'post one', 'description' => 'd1...', 'status' => '1'],
+            ['id' => 2, 'title' => 'post two', 'description' => 'd2...', 'status' => '0'],
+            ['id' => 3, 'title' => 'post three', 'description' => 'd3...', 'status' => '0'],
+        ];
+        Post::insert($var);
+
     }
 }
