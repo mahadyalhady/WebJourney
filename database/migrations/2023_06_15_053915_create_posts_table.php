18eaf887944ee380663e9e1dc583f55fe483801b
Mahady Al Hady <mahady426@gmail.com>
2023-06-17 04:38:36 +0600

    Update 2023_06_15_053915_create_posts_table.php

--- database/migrations/2023_06_15_053915_create_posts_table.php
+++ database/migrations/2023_06_15_053915_create_posts_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->tinyInteger('status')->default('1');
             $table->text('description')->nullable();
             $table->timestamps();
 
